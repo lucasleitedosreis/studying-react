40e5dccb56761e072b1a2cf8dd89d93c8c7dbd1d
LUCAS LEITE DOS REIS <lucas.leitedosreis@gmail.com>
2023-05-31 00:29:29 -0300

    feat: Exercício 2 de Arrays com React

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,12 @@
 import React from "react";
-// Mostre os dados da aplicação, como apresentado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
-const dados = luana;
-
-const total = dados.compras
-  .map((compra) => {
-    return Number(compra.preco.replace("R$ ", ""));
-  })
-  .reduce((a, b) => a + b);
+// import Exercicios01Jsx from "./assets/exercicios/Exercicios01Jsx";
+import Exercicios02Arrays from "./assets/exercicios/Exercicios02Arrays";
 
 function App() {
   return (
     <>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{" "}
-        <span style={{ color: dados.ativa ? "green" : "red" }}>
-          {dados.ativa ? "Ativo" : "Inativa"}{" "}
-        </span>
-      </p>
-      <p>Total gasto: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito.</p>}
+      {/* <Exercicios01Jsx /> */}
+      <Exercicios02Arrays />
     </>
   );
 }
---
+++ src/assets/exercicios/Exercicios02Arrays.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
+export default function Exercicios02Arrays() {
+  const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace("R$ ", "")) > 1500
+  );
+  console.log(dados);
+  return (
+    <section>
+      {dados.map(({ nome, cores, id, preco }) => (
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>{preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li key={cor} style={{ backgroundColor: cor, color: "white" }}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}{" "}
+    </section>
+  );
+}
