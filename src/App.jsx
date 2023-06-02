a5aa898cc1ef463dbc74a1de10bc5e5f815b1ab5
LUCAS LEITE DOS REIS <lucas.leitedosreis@gmail.com>
2023-06-02 14:45:14 -0300

    style: organizando arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,6 @@
 import React from "react";
-import Header from "./exercicios/exercicios03/Header";
-import Home from "./exercicios/exercicios03/Home";
-import Produtos from "./exercicios/exercicios03/Produtos";
-// import Exercicios01Jsx from "./assets/exercicios/Exercicios01Jsx";
-// import Exercicios02Arrays from "./assets/exercicios/Exercicios02Arrays";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Form from "./components/Form/Form";
-// import Titulo from "./components/Titulo";
-// import Form2 from "./components/Form2/Form2";
 
 function App() {
-  let Pagina = Home;
-  const { pathname } = window.location;
-  if (pathname === "/produtos") {
-    Pagina = Produtos;
-  } else {
-    Pagina === Home;
-  }
-  return (
-    <>
-      {/* <Exercicios01Jsx /> */}
-      {/* <Exercicios02Arrays /> */}
-
-      {/* components , propriedades*/}
-      {/* --------------------------------------------------------- */}
-      {/* <Header />
-      <Titulo texto="Seja Bem Vindo" />
-      <Titulo cor="red" texto="Seja Bem Vindo" />
-      <Titulo cor="green">Children</Titulo>
-      <Form />
-      <Form2 />
-      <Footer /> */}
-      {/* --------------------------------------------------------- */}
-      {/* Exercício 3 */}
-      <Header />
-      <Pagina />
-    </>
-  );
+  return <></>;
 }
 export default App;
---
+++ src/exercicios/Exercicios03Componentes/Exercicios03Componentes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Home from "./Home";
+import Produtos from "./Produtos";
+
+export default function Exercicios03Componentes() {
+  let Pagina = Home;
+  const { pathname } = window.location;
+  if (pathname === "/produtos") {
+    Pagina = Produtos;
+  } else {
+    Pagina === Home;
+  }
+  return (
+    <>
+      <Header />
+      <Pagina />
+    </>
+  );
+}
